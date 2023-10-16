9b24e17d80ac2376316a6c10ab9a59dec7152b2c
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2023-10-17 00:08:49 +0200

    progress (still needs work)

--- cogs/music.py
+++ cogs/music.py
@@ -3,20 +3,31 @@
 #     Music     #
 #               #
 #################
-
 import os
 import platform
+import dotenv
+import threading
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-from builtins import bot
-
 import yt_dlp
+
+import spotipy
+from spotipy import SpotifyOAuth
 
 import asyncio
 import ctypes.util
+
+from cogs.utils.music_helper import YTDLPCMVolumeTransformer, MusicPlayer
+
+
+SPOTIPY_CLIENT_ID = dotenv.get_key('.env', 'spotify_client_id')
+SPOTIPY_CLIENT_SECRET = dotenv.get_key('.env', 'spotify_client_secret')
+SPOTIPY_REDIRECT_URI = dotenv.get_key('.env', 'spotify_redirect_uri')
+
+sp = spotipy.Spotify(auth_manager = SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI))
 
 
 # print(platform.system())
@@ -24,149 +35,100 @@ if platform.system() != 'Windows':
     find_opus = ctypes.util.find_library('opus')
     discord.opus.load_opus(find_opus)
 
-yt_dlp.utils.bug_reports_message = lambda: ''
-ytdl_logs = False
-
-
-class YTDLError(Exception):
-    pass
-
-
-class VoiceError(Exception):
-    pass
-
-
-class YTDLPCMVolumeTransformer(discord.PCMVolumeTransformer):
-    # YTDL settings
-    YTDL_OPTIONS = {
-        'logtostderr': ytdl_logs,
-        'format': 'm4a/bestaudio/best',
-        'restrictfilenames': True,
-        'no_warnings': True,
-        'default_search': 'auto',
-        'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
-        'postprocessors': [{  # Extract audio using ffmpeg
-            'key': 'FFmpegExtractAudio'
-            # 'preferredcodec': 'm4a',
-        }]
-    }
-
-    # FFMPEG settings
-    FFMPEG_OPTIONS = {
-        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn'
-    }
-
-    ytdl = yt_dlp.YoutubeDL(YTDL_OPTIONS)
-
-    def __init__(self, interaction: discord.Interaction, audio_source: discord.FFmpegPCMAudio, info: dict):
-        super().__init__(audio_source)
-
-        self.audio_source = audio_source
-        self.requester = interaction.user
-        self.channel = interaction.channel
-        self.info = info
-
-        self.uploader: str = info.get('uploader')
-        self.title: str = info.get('title')
-        self.thumbnail = info.get('thumbnail')
-        self.raw_duration: int = info.get('duration')
-        self.str_duration: str = self.get_duration(int(info.get('duration')))
-        self.url: str = info.get('webpage_url')
-        self.stream_url: str = info.get('url')
-
-    def __str__(self):
-        return f"{self.title}, requested by {self.requester.mention}"
-
-    @classmethod
-    def create_source(cls, interaction: discord.Interaction, search: str):
-        print('cs')
-        try:
-            domains = ["www.youtube.com/", "music.youtube.com/", "youtu.be/", "open.spotify.com/", "spotify.com/"]
-            if search.startswith('https://'):
-                for domain in domains:
-                    if search.startswith('https://' + domain):
-                        print('youtube/spotify')
-                        info = cls.ytdl.extract_info(search, download = False)['entries'][0]
-
-            else:
-                print('generic')
-                info = cls.ytdl.extract_info(f'ytsearch:{search}', download = False)['entries'][0]
-
-        except YTDLError as err:
-            print(err)
-            return interaction.followup.send('Something went wrong during the content search!', ephemeral = True)
-        return cls(interaction, discord.FFmpegPCMAudio(info.get('url'), **cls.FFMPEG_OPTIONS), info)
-
-    @staticmethod
-    def get_duration(duration: int):
-        minutes, seconds = divmod(duration, 60)
-        hours, minutes = divmod(minutes, 60)
-        days, hours = divmod(hours, 24)
-
-        duration = []
-        if days > 0:
-            duration.append(f'{days} days')
-        if hours > 0:
-            duration.append(f'{hours} hours')
-        if minutes > 0:
-            duration.append(f'{minutes} minutes')
-        if seconds > 0:
-            duration.append(f'{seconds} seconds')
-
-        return ', '.join(duration)
-
-
-class MusicPlayer:
-    def __init__(self, bot):
-        self.bot = bot
-        self.voice_client = None
-        self.queue = []     # Inizialize an empty queue to store song
-        self.loop = False
-        self.current_song = None
-
-    def get_voice_client(self, guild: discord.Guild):
-        return discord.utils.get(self.bot.voice_clients, guild = guild)
-
-    async def disconnect(self):
-        if self.voice_client and self.voice_client.is_connected():
-            self.voice_client.stop()
-            await self.voice_client.disconnect()
-
-    async def stop(self):
-        self.voice_client.stop()
-
-    def add_song_to_queue(self, song):
-        self.queue.append(song)
-
-    async def play_next_song(self):
-        print('playing next song')
-        if self.queue:
-            print('self.queue')
-            print(f'playing next song')
-            song = self.queue.pop(0)
-            print(song.title)
-            self.current_song = song
-            self.voice_client.play(song, after = self.play_next_song)
-            await self.wait_for_song_end()
-        elif self.loop:
-            print('self.loop')
-            song = self.current_song
-            self.voice_client.play(song, after = self.play_next_song)
-
-    async def wait_for_song_end(self):
-        while self.voice_client.is_playing() or self.voice_client.is_paused():
-            await asyncio.sleep(1)
-
-    async def start_playing(self):
-        while self.queue:
-            await self.play_next_song()
-
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.music_player = MusicPlayer(bot)
         self.bot = bot
+
+    def start_playing_in_thread(self):
+        asyncio.run(self.music_player.start_playing())
+
+    async def play_youtube_search(self, interaction: discord.Interaction, search: str):
+        """Generic request searches from YouTube."""
+        song = YTDLPCMVolumeTransformer.create_source(interaction, search)
+        self.music_player.add_song_to_queue(song)
+        await interaction.followup.send(f'Added to queue: **{song.title}**')
+
+        if not self.music_player.voice_client.is_playing():
+            await self.music_player.start_playing()
+
+    async def play_individual_youtube_video(self, interaction: discord.Interaction, video_url: str):
+        """Plays/Adds to the queue a single YouTube video"""
+        song = YTDLPCMVolumeTransformer.create_source(interaction, video_url)
+        self.music_player.add_song_to_queue(song)
+        await interaction.followup.send(f'Added to the queue: **{song.title}**')
+
+        if not self.music_player.voice_client.is_playing():
+            await self.music_player.start_playing()
+
+    async def play_individual_spotify_track(self, interaction: discord.Interaction, track_url: str):
+        """Plays/Adds to the queue a single Spotify track"""
+        song = YTDLPCMVolumeTransformer.create_source(interaction, track_url)
+        self.music_player.add_song_to_queue(song)
+        await interaction.followup.send(f'Added to the queue: **{song.title}**')
+
+        if not self.music_player.voice_client.is_playing():
+            await self.music_player.start_playing()
+
+    async def play_youtube_playlist(self, interaction: discord.Interaction, playlist_url: str):
+        """Plays/Adds to the queue a YouTube playlist"""
+
+        # Define custom YTDL options for playlist
+        playlist_ytdl_options = {
+            'format': 'bestaudio/best',
+            'quiet': True,
+            'extract_flat': True,
+            'skip_download': True  # This option skips the download step for playlists
+        }
+
+        channel = interaction.channel
+
+        with yt_dlp.YoutubeDL(playlist_ytdl_options) as ydl:
+            playlist_info = ydl.extract_info(playlist_url, download = False)
+
+            if 'entries' in playlist_info:
+                entries = playlist_info['entries']
+
+                if not entries:
+                    await interaction.followup.send('No tracks found in the YouTube playlist.')
+                    return
+
+                await interaction.followup.send(f'Added {len(entries)} tracks from the YouTube playlist.')
+
+                # Add each song in the playlist to the queue
+                for entry in entries:
+                    song_url = entry['url']
+                    song = YTDLPCMVolumeTransformer.create_source(interaction, song_url)
+                    self.music_player.add_song_to_queue(song)
+
+                    if not self.music_player.voice_client.is_playing() and not self.music_player.voice_client.is_paused():
+                        playback_thread = threading.Thread(target = self.start_playing_in_thread)
+                        playback_thread.start()
+                        # await channel.send(f'Started playing the first track from the YouTube playlist.')
+
+            else:
+                await channel.send('No tracks found in the YouTube playlist.')
+
+    async def play_spotify_playlist(self, interaction: discord.Interaction, playlist_url: str):
+        """Plays/Adds to the queue a Spotify playlist"""
+        playlist_id = playlist_url.split('/')[-1]
+        tracks = sp.playlist_items(playlist_id)
+
+        if tracks and tracks['items']:
+            for track in tracks['items']:
+                track_url = track['track']['external_urls']['spotify']
+                print(track_url)
+                song = YTDLPCMVolumeTransformer.create_source(interaction, track_url)
+                self.music_player.add_song_to_queue(song)
+
+                if not self.music_player.voice_client.is_playing():
+                    await self.music_player.start_playing()
+
+            await interaction.followup.send(f'Added {len(tracks["items"])} from the Spotify playlist to the queue.')
+
+        else:
+            await interaction.followup.send('No tracks found in the Spotify playlist.')
 
     @app_commands.command(name = 'play')
     async def play(self, interaction: discord.Interaction, search: str):
@@ -178,14 +140,26 @@ class Music(commands.Cog):
             channel = interaction.user.voice.channel
             self.music_player.voice_client = await channel.connect()
 
-        print('creating source and song')
-        song = YTDLPCMVolumeTransformer.create_source(interaction, search)
-        # self.music_player.add_song_to_queue(song)
-        self.music_player.add_song_to_queue(YTDLPCMVolumeTransformer.create_source(interaction, search))
-        await interaction.followup.send(f'Added to the queue: **{song.title}**')
-
-        if not self.music_player.voice_client.is_playing():
-            await self.music_player.start_playing()
+        if search.startswith('https://open.spotify.com/playlist/'):
+            # Spotify playlist
+            print('Spotify playlist')
+            await self.play_spotify_playlist(interaction, search)
+        elif search.startswith('https://www.youtube.com/playlist?'):
+            # YouTube playlist
+            print('YouTube playlist')
+            await self.play_youtube_playlist(interaction, search)
+        elif search.startswith('https://open.spotify.com/track/'):
+            # Individual Spotify track
+            print('Spotify track')
+            await self.play_individual_spotify_track(interaction, search)
+        elif search.startswith('https://www.youtube.com/watch?'):
+            # Individual YouTube video
+            print('YouTube video')
+            await self.play_individual_youtube_video(interaction, search)
+        else:
+            # Generic search (YouTube)
+            print('Generic search (YouTube)')
+            await self.play_youtube_search(interaction, search)
 
     @app_commands.command(name = 'stop')
     async def stop(self, interaction: discord.Interaction):
@@ -196,12 +170,16 @@ class Music(commands.Cog):
             await interaction.response.send_message('The bot is not connected to a voice channel.', ephemeral = True)
 
     @app_commands.command(name = 'queue')
-    async def queue(self, interaction: discord.Interaction):
-        queue_message = '\n'.join(f'{index + 1}. {song.title}' for index, song in enumerate(self.music_player.queue))
-        if queue_message:
-            await interaction.response.send_message(f'Queue:\n{queue_message}')
-        else:
+    async def show_queue(self, interaction: discord.Interaction):
+        print("show_queue command executed")
+
+        if not self.music_player.queue:
+            print('queue if')
             await interaction.response.send_message('The queue is empty.')
+            return
+
+        queue_message = '\n'.join(f'{index + 1}. {song.title}' for index, song in enumerate(self.music_player.queue))
+        await interaction.response.send_message(f'Queue:\n{queue_message}')
 
     @app_commands.command(name = 'disconnect')
     async def disconnect(self, interaction: discord.Interaction):
---
+++ cogs/utils/music_helper.py
@@ -0,0 +1,137 @@
+import dotenv
+
+import discord
+
+import yt_dlp
+
+import asyncio
+
+
+yt_dlp.utils.bug_reports_message = lambda: ''
+ytdl_logs = False
+
+
+class YTDLError(Exception):
+    pass
+
+
+class VoiceError(Exception):
+    pass
+
+
+class YTDLPCMVolumeTransformer(discord.PCMVolumeTransformer):
+    # YTDL settings
+    YTDL_OPTIONS = {
+        'logtostderr': ytdl_logs,
+        'format': 'm4a/bestaudio/best',
+        'restrictfilenames': True,
+        'no_warnings': True,
+        'default_search': 'auto',
+        'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio'
+            # 'preferredcodec': 'm4a',
+        }]
+    }
+
+    # FFMPEG settings
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+        'options': '-vn'
+    }
+
+    ytdl = yt_dlp.YoutubeDL(YTDL_OPTIONS)
+
+    def __init__(self, interaction: discord.Interaction, audio_source: discord.FFmpegPCMAudio, info: dict):
+        super().__init__(audio_source)
+
+        self.audio_source = audio_source
+        self.requester = interaction.user
+        self.channel = interaction.channel
+        self.info = info
+
+        self.uploader: str = info.get('uploader')
+        self.title: str = info.get('title')
+        self.thumbnail = info.get('thumbnail')
+        self.raw_duration: int = info.get('duration')
+        self.str_duration: str = self.get_duration(int(info.get('duration')))
+        self.url: str = info.get('webpage_url')
+        self.stream_url: str = info.get('url')
+
+    def __str__(self):
+        return f"{self.title}, requested by {self.requester.mention}"
+
+    @classmethod
+    def create_source(cls, interaction: discord.Interaction, search: str):
+        print('cs')
+        try:
+            print('try')
+            info = cls.ytdl.extract_info(f'ytsearch:{search}', download = False)['entries'][0]
+        except YTDLError as err:
+            print(err)
+            return interaction.followup.send('Something went wrong during the content search!', ephemeral = True)
+        return cls(interaction, discord.FFmpegPCMAudio(info.get('url'), **cls.FFMPEG_OPTIONS), info)
+
+    @staticmethod
+    def get_duration(duration: int):
+        minutes, seconds = divmod(duration, 60)
+        hours, minutes = divmod(minutes, 60)
+        days, hours = divmod(hours, 24)
+
+        duration = []
+        if days > 0:
+            duration.append(f'{days} days')
+        if hours > 0:
+            duration.append(f'{hours} hours')
+        if minutes > 0:
+            duration.append(f'{minutes} minutes')
+        if seconds > 0:
+            duration.append(f'{seconds} seconds')
+
+        return ', '.join(duration)
+
+
+class MusicPlayer:
+    def __init__(self, bot):
+        self.bot = bot
+        self.voice_client = None
+        self.queue = []     # Inizialize an empty queue to store song
+        self.loop = False
+        self.current_song = None
+
+    def get_voice_client(self, guild: discord.Guild):
+        return discord.utils.get(self.bot.voice_clients, guild = guild)
+
+    async def disconnect(self):
+        if self.voice_client and self.voice_client.is_connected():
+            self.voice_client.stop()
+            await self.voice_client.disconnect()
+
+    async def stop(self):
+        self.voice_client.stop()
+
+    def add_song_to_queue(self, song):
+        self.queue.append(song)
+
+    async def play_next_song(self):
+        print('playing next song')
+        if self.queue:
+            print('self.queue')
+            print(f'playing next song')
+            song = self.queue.pop(0)
+            print(song.title)
+            self.current_song = song
+            self.voice_client.play(song, after = self.play_next_song)
+            await self.wait_for_song_end()
+        elif self.loop:
+            print('self.loop')
+            song = self.current_song
+            self.voice_client.play(song, after = self.play_next_song)
+
+    async def wait_for_song_end(self):
+        while self.voice_client.is_playing() or self.voice_client.is_paused():
+            await asyncio.sleep(1)
+
+    async def start_playing(self):
+        while self.queue:
+            await self.play_next_song()
